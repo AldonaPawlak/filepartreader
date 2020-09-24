6a192e0b7a36eac28ba339099734e0c8eabc8e9d
Aldona <aldonapawlak2@gmail.com>
2020-09-24 17:45:43 +0200

    implement constructor and setup methods

--- src/main/java/com/codecool/FilePartReader.java
+++ src/main/java/com/codecool/FilePartReader.java
@@ -9,9 +9,18 @@ public class FilePartReader {
     private Integer toLine;
 
     public FilePartReader() {
+        this.filePath = "";
+        this.fromLine = 0;
+        this.toLine = 0;
     }
 
     public void setup(String filePath, Integer fromLine, Integer toLine) throws IllegalArgumentException {
+        if (toLine < fromLine || fromLine < 1) {
+            throw new IllegalArgumentException();
+        }
+        this.filePath = filePath;
+        this.fromLine = fromLine;
+        this.toLine = toLine;
     }
 
     public String read(String filePath) throws IOException {
