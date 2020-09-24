85896580989b65569ff68c51e583fb69bc2b9bb6
Aldona <aldonapawlak2@gmail.com>
2020-09-24 17:41:15 +0200

    create methods signatures

--- src/main/java/com/codecool/FilePartReader.java
+++ src/main/java/com/codecool/FilePartReader.java
@@ -1,4 +1,25 @@
 package com.codecool;
 
+import java.io.IOException;
+
 public class FilePartReader {
+
+    private String filePath;
+    private Integer fromLine;
+    private Integer toLine;
+
+    public FilePartReader() {
+    }
+
+    public void setup(String filePath, Integer fromLine, Integer toLine) throws IllegalArgumentException {
+    }
+
+    public String read(String filePath) throws IOException {
+        return null;
+    }
+
+    public String readLines() {
+
+        return null;
+    }
 }
---
+++ src/main/java/com/codecool/FileWordAnalyzer.java
@@ -0,0 +1,23 @@
+package com.codecool;
+
+import java.util.List;
+
+public class FileWordAnalyzer {
+
+    private FilePartReader filePartReader;
+
+    public FileWordAnalyzer() {
+    }
+
+    public List<String> getWordsOrderedAlphabetically () {
+        return null;
+    }
+
+    public List<String> getWordsContainingSubstring (String subString ) {
+        return null;
+    }
+
+    public List<String> getStringsWhichPalindromes () {
+        return null;
+    }
+}
